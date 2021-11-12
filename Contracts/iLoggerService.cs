05f91dfb025bf0e34ec5fcaa9b65b31c7216e41d
getsl <getsl@DESKTOP-MU5TDIL>
2021-11-12 04:37:53 +0200

    Add project files.

---
+++ Contracts/iLoggerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_APP.Contracts
+{
+    public interface ILoggerService
+    {
+        void LogInfo(string message);
+
+        void LogWarn(string message);
+
+        void LogDebug(string message);
+
+        void LogError(string message);
+
+    }
+}
